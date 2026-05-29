dbeb42fb37e1b0e9053794797c08b8b56fb0dc86
ikeguimaraes-dot <ikeguimaraes@gmail.com>
2026-05-29 20:37:48 -0300

    fix: metro config para transpilar supabase realtime private properties

--- metro.config.js
+++ metro.config.js
@@ -1,2 +1,9 @@
-const { getDefaultConfig } = require("expo/metro-config");
-module.exports = getDefaultConfig(__dirname);
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.transformer.transformIgnorePatterns = [
+  'node_modules/(?!((@react-native|react-native|expo|@expo|@expo/vector-icons|@supabase/realtime-js|@supabase/supabase-js)/|sentry-expo/|native-base/|react-native-svg/))',
+];
+
+module.exports = config;
